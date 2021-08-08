1588081d4b985140d06ae8113b3ae569c78dafbb
Arturo0911 <anegreiross@outlook.com>
2021-08-08 00:59:09 -0500

    adding dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root_payload:@localhost/PureMango'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 CORS(app)
 
 from app import routes
