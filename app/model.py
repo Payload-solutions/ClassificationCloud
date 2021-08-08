c6e8a21a89ecec9034fc21482c8d1c4b9d2dcec4
Arturo0911 <anegreiross@outlook.com>
2021-08-08 01:50:54 -0500

    adding dependencies

--- app/model.py
+++ app/model.py
@@ -1,8 +1,11 @@
 
-from app import app
+from operator import index
+from app import db
+import pandas as pd
+
 
 class YogurtData(db.Model):
-    __tablename__ = "YogurtData"
+    __tablename__ = "yogurt_features"
     features_id = db.Column(db.Integer, primary_key=True)
     streptococcus_initial = db.Column(db.Float, index=True, nullable=False)
     lactobacillus_initial = db.Column(db.Float, index=True, nullable=False)
@@ -14,6 +17,13 @@ class YogurtData(db.Model):
     quality_product = db.Column(db.String(20), index=True, nullable=False)
     streptococcus_final = db.Column(db.Float, index=True, nullable=False)
     lactobacillus_final = db.Column(db.Float, index=True, nullable=False)
-
+    quality_product_category = db.Column(db.Integer, index=True, nullable=False)
 
 db.create_all()
+
+
+def model_exists() -> bool:
+    if not db.Query("yogurt_features").first():
+        dataframe = pd.read_csv("classification_data.csv")
+        dataframe.to_sql(name = "yogurt_features", con=db.engine, index=False)
+model_exists()
