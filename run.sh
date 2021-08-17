626a09e793a8043445132058b7682f7af91ad66e
Arturo0911 <anegreiross@outlook.com>
2021-08-17 01:45:42 -0500

    ok

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 sudo systemctl start mysql.service
-
-
-MYSQL_DATABASE: YogurtQuality
-            MYSQL_USER: payload
-            MYSQL_ROOT_PASSWORD: root
-            MYSQL_PASSWORD: payload123
