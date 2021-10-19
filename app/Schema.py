6adede7335fe758203566ba59662eb6b8ba381f1
Arturo0911 <anegreiross@outlook.com>
2021-10-19 18:54:09 -0500

    ok

--- app/Schema.py
+++ app/Schema.py
@@ -1,21 +1,21 @@
-from app import ma
+# from app import ma
 
 
-class YogurtSchema(ma.Schema):
+# class YogurtSchema(ma.Schema):
 
-    class Meta:
-        fields = ("index","streptococcus_initial_strain_cfu_ml",
-                  "lactobacillus_initial_strain_cfu_ml",
-                  "ideal_temperature_c",
-                  "minimum_milk_proteins",
-                  "titratable_acidity",
-                  "pH_milk_sour_",
-                  "fat_milk_over_100mg_",
-                  "quality_product",
-                  "lactobacillus_final_cfu_ml",
-                  "streptococcus_final_cfu_ml",
-                  "quality_product_")
+#     class Meta:
+#         fields = ("index","streptococcus_initial_strain_cfu_ml",
+#                   "lactobacillus_initial_strain_cfu_ml",
+#                   "ideal_temperature_c",
+#                   "minimum_milk_proteins",
+#                   "titratable_acidity",
+#                   "pH_milk_sour_",
+#                   "fat_milk_over_100mg_",
+#                   "quality_product",
+#                   "lactobacillus_final_cfu_ml",
+#                   "streptococcus_final_cfu_ml",
+#                   "quality_product_")
 
-yogurt_schema = YogurtSchema()
-yogurt_schemas = YogurtSchema(many=True)
+# yogurt_schema = YogurtSchema()
+# yogurt_schemas = YogurtSchema(many=True)
 
--- app/routes.py
+++ app/routes.py
@@ -5,20 +5,20 @@ from flask import (
     request
 )
 import unittest
-from app.model import (
-    YogurtData
-)
-from app.Schema import (
-    yogurt_schemas,
-    yogurt_schema
-)
+# from app.model import (
+#     YogurtData
+# )
+# from app.Schema import (
+#     yogurt_schemas,
+#     yogurt_schema
+# )
 from pprint import pprint
 
 
-@app.cli.command()
-def tst():
-    tests = unittest.TestLoader().discover("tests")
-    unittest.TextTestRunner().run(tests)
+# @app.cli.command()
+# def tst():
+#     tests = unittest.TestLoader().discover("tests")
+#     unittest.TextTestRunner().run(tests)
 
 
 @app.errorhandler(400)
@@ -88,72 +88,72 @@ def internal_server_error(error):
 
 # Normal methods
 
-@app.route("/", methods=["GET"])
-def index():
+# @app.route("/", methods=["GET"])
+# def index():
 
-    data = YogurtData.query.all()
-    elements = yogurt_schemas.dump(data)
+#     data = YogurtData.query.all()
+#     elements = yogurt_schemas.dump(data)
 
-    return jsonify(elements)
-
-
-@app.route("/one_data/<index>", methods=["GET"])
-def get_one_feature(index):
-    data = YogurtData.query.get(index)
-    return yogurt_schema.jsonify(data)
+#     return jsonify(elements)
 
 
-@app.route("/new_feature", methods=["POST"])
-def new_feature():
-    try:
+# @app.route("/one_data/<index>", methods=["GET"])
+# def get_one_feature(index):
+#     data = YogurtData.query.get(index)
+#     return yogurt_schema.jsonify(data)
+
+
+# @app.route("/new_feature", methods=["POST"])
+# def new_feature():
+#     try:
         
-        # data = {
-        #     "streptococcus_initial_strain_cfu_ml":3.683,
-        #     "lactobacillus_initial_strain_cfu_ml":4.6979999999999995,
-        #     "ideal_temperature_c":41.681999999999995,
-        #     "minimum_milk_proteins":2.67,
-        #     "titratable_acidity":1.001,
-        #     "pH_milk_sour_":4.577,
-        #     "fat_milk_over_100mg_":3.8438,
-        #     "quality_product":"Regular yogurt",
-        #     "lactobacillus_final_cfu_ml":9.56,
-        #     "streptococcus_final_cfu_ml":7.86,
-        #     "quality_product_":2,
-        # }
-        print(request.json)
-        data_insertion = request.json
-        new_insertion = YogurtData(
-                float(data_insertion["streptococcus_initial_strain_cfu_ml"]),
-                float(data_insertion["lactobacillus_initial_strain_cfu_ml"]),
-                float(data_insertion["ideal_temperature_c"]),
-                float(data_insertion["minimum_milk_proteins"]),
-                float(data_insertion["titratable_acidity"]),
-                float(data_insertion["pH_milk_sour_"]),
-                float(data_insertion["fat_milk_over_100mg_"]),
-                (data_insertion["quality_product"]),
-                float(data_insertion["lactobacillus_final_cfu_ml"]),
-                float(data_insertion["streptococcus_final_cfu_ml"]),
-                int(data_insertion["quality_product_"])
-        )
-        db.session.add(new_insertion)
-        db.session.commit()
-        return jsonify({
-            "message": "saved into database successfully"
-        })
-    except Exception as e:
-        print(str(e))
-        return jsonify({
-            "message": "Error by: {}".format(str(e))
-        })
+#         # data = {
+#         #     "streptococcus_initial_strain_cfu_ml":3.683,
+#         #     "lactobacillus_initial_strain_cfu_ml":4.6979999999999995,
+#         #     "ideal_temperature_c":41.681999999999995,
+#         #     "minimum_milk_proteins":2.67,
+#         #     "titratable_acidity":1.001,
+#         #     "pH_milk_sour_":4.577,
+#         #     "fat_milk_over_100mg_":3.8438,
+#         #     "quality_product":"Regular yogurt",
+#         #     "lactobacillus_final_cfu_ml":9.56,
+#         #     "streptococcus_final_cfu_ml":7.86,
+#         #     "quality_product_":2,
+#         # }
+#         print(request.json)
+#         data_insertion = request.json
+#         new_insertion = YogurtData(
+#                 float(data_insertion["streptococcus_initial_strain_cfu_ml"]),
+#                 float(data_insertion["lactobacillus_initial_strain_cfu_ml"]),
+#                 float(data_insertion["ideal_temperature_c"]),
+#                 float(data_insertion["minimum_milk_proteins"]),
+#                 float(data_insertion["titratable_acidity"]),
+#                 float(data_insertion["pH_milk_sour_"]),
+#                 float(data_insertion["fat_milk_over_100mg_"]),
+#                 (data_insertion["quality_product"]),
+#                 float(data_insertion["lactobacillus_final_cfu_ml"]),
+#                 float(data_insertion["streptococcus_final_cfu_ml"]),
+#                 int(data_insertion["quality_product_"])
+#         )
+#         db.session.add(new_insertion)
+#         db.session.commit()
+#         return jsonify({
+#             "message": "saved into database successfully"
+#         })
+#     except Exception as e:
+#         print(str(e))
+#         return jsonify({
+#             "message": "Error by: {}".format(str(e))
+#         })
 
-@app.route("/new_features", methods=["POST"])
-def new_features():
-    try:
-        pass
-    except Exception as e:
-        return jsonify({
-            "message": "Error by: {}".format(str(e))
-        })
+# @app.route("/new_features", methods=["POST"])
+# def new_features():
+#     try:
+#         pass
+#     except Exception as e:
+#         return jsonify({
+#             "message": "Error by: {}".format(str(e))
+#         })
 
 
 
